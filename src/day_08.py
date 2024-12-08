d3b817377b9a107e45729a46be014d0f68c04abe
evallina <eloyvallina33@gmail.com>
2024-12-08 16:36:35 +0100

    Part 2

--- src/day_08.py
+++ src/day_08.py
@@ -34,7 +34,6 @@ def part1(data):
         map = np.array(
             [list(l.strip()) for l in f.readlines()]
         )
-        original = [row.copy() for row in map.copy()]
 
     MAPWIDTH = len(map[0])
     MAPHEIGHT = len(map)
@@ -62,3 +61,35 @@ print("Part 1:", len(part1(actual)))
 
 
 
+def part2(data):
+    with open(data, 'r') as f:
+        map = np.array(
+            [list(l.strip()) for l in f.readlines()]
+        )
+
+
+    MAPHEIGHT = len(map[0])
+    MAPWIDTH = len(map)
+
+    antinodes = []
+    for kind in np.unique(map[map != '.']):
+        ii, jj = np.where(map == kind)
+        antenna = [(int(i), int(j)) for i, j in zip(ii, jj)]
+        for a, b in permutations(antenna, 2):
+            for factor in range(0, MAPWIDTH):
+                iup = a[0] - factor*(b[0] - a[0])
+                jback = a[1] - factor*(b[1] - a[1])
+                idown = b[0] + factor*(b[0] - a[0])
+                jforth = b[1] + factor*(b[1]- a[1])
+
+                if 0 <= iup < MAPHEIGHT and 0 <= jback < MAPWIDTH:
+                    # antinode in map
+                    antinodes.append((iup, jback))
+            
+                if 0 <= idown < MAPHEIGHT and 0 <= jforth < MAPWIDTH: 
+                    antinodes.append((idown, jforth))
+
+    return set(antinodes)
+
+assert len(part2(example)) == 34
+print("Part 2:", len(part2(actual)))
