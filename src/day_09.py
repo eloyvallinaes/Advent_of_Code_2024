c3c233b972eeb73e467b204d053120e0aaaf0449
Eloy <eloyvallina33@gmail.com>
2024-12-09 17:07:07 +0100

    Part 2

--- src/day_09.py
+++ src/day_09.py
@@ -9,6 +9,11 @@ Compact hard drive and calculate checksum.
     with the file ID number it contains.
 
 Part 2:
+Compact the disk by moving whole files instead.
+    - Attempt to move each file exactly once starting with the file with the
+    highest file ID number.
+    - If there is no span of free space to the left of a file that is large
+    enough to fit the file, the file does not move.
 
 
 """
@@ -17,11 +22,11 @@ example = 'inputs/ex_09.txt'
 actual = 'inputs/day_09.txt'
 
 
-def checksum(long):
-    return sum(i*int(e) for i, e in enumerate(long))
-
-
 def part1(data):
+
+    def checksum(long):
+        return sum(i*int(e) for i, e in enumerate(long))
+
     drive = open(data, 'r').read().strip()
 
     sizes = []
@@ -43,12 +48,11 @@ def part1(data):
     # before compacting
     # print(''.join([str(e) for e in long]))
 
-    original = long.copy()
     for space in spaces:
         if space > 0:
             try:
                 free = long.index(".")  # first free block
-            except ValueError:  # no more space 
+            except ValueError:  # no more space
                 break
             tail = long[free+space:][::-1]
             trim = space
@@ -64,9 +68,73 @@ def part1(data):
     # print(  # after compacting
     #     ''.join([str(e) for e in long])
     # )
-
     return checksum(long)
 
 
 part1(example) == 1928
-print("Part 1:", part1(actual))
+# print("Part 1:", part1(actual))
+
+
+def part2(data):
+
+    def checksum(long):
+        return sum(i*int(e) for i, e in enumerate(long) if e != ".")
+
+    drive = open(actual, 'r').read().strip()
+    sizes = []
+    spaces = []
+    for i in range(0, len(drive), 2):
+        sizes.append(int(drive[i]))
+        try:
+            spaces.append(int(drive[i+1]))
+        except IndexError:  # no free space after last file
+            spaces.append(0)
+            break
+
+    fileindex = list(range(0, len(sizes)))
+
+    long = []
+    for i, fileid in enumerate(fileindex):
+        long += [fileid] * sizes[i] + ['.'] * spaces[i]
+
+    # before compacting
+    # print(''.join([str(e) for e in long]))
+    for fileid, size in zip(fileindex[::-1], sizes[::-1]):
+        filestart = long.index(fileid)
+        fileend = filestart + size
+        scan = long.copy()
+        offset = 0
+        while True:
+            try:
+                free = scan.index(".")
+            except ValueError:
+                break
+
+            if free+offset >= filestart:
+                break
+
+            span = 1
+            while set(scan[free:free+span]) == set("."):
+                span += 1
+
+            if span-1 >= size:
+                head = long[:offset+free]
+                middle = long[offset+free+size:filestart]
+                newgap = ["."] * size
+                tail = long[fileend:]
+                long = head + [fileid] * size + middle + newgap + tail
+                break
+            else:
+                offset += free+span
+                scan = long[offset:]
+
+    # print(  # after compacting
+    #     ''.join([str(e) for e in long])
+    # )
+
+    return checksum(long)
+
+
+assert part2(example) == 2858
+print('Calculating Part 2, this takes a while...')
+print("Part 2:", part2(actual))
