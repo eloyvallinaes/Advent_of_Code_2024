db61d9765476434fe153708e24f073e8c2059a4a
evallina <eloyvallina33@gmail.com>
2024-12-12 19:53:12 +0100

    Day 11 part 2 WIP

--- src/day_11.py
+++ src/day_11.py
@@ -16,59 +16,43 @@ example = 'inputs/ex_11.txt'
 actual = 'inputs/day_11.txt'
 
 
-def part1(data, n):
-    stones = open(data, 'r').read().strip().split()  # string
-    for _ in range(n):
-        newline = []
-        for stone in stones:
-            if int(stone) == 0:
-                newline += ["1"]
-            elif len(stone) % 2 == 0:
-                head = stone[:len(stone)//2]
-                tail = stone[len(stone)//2:]
-                newline += [str(int(head))] + [str(int(tail))]
-            else:
-                newline += [str(int(stone) * 2024)]
+def blink(stone, memo=None):
+    if memo is None:
+        memo = {
+            "0": ["1"],
+        }
 
-        stones = newline.copy()
-
-    return len(stones)
-
-
-def part2(stones, n):
-
-    def blink(stone, memo=None):
-        if memo is None:
-            memo = {
-                "0": ["1"],
-            }
-
-        if stone in memo:
-            return memo[stone]
-
-        if len(stone) % 2 == 0:
-            head = stone[:len(stone)//2]
-            tail = stone[len(stone)//2:]
-            memo.update({
-                stone: [blink(str(int(head)), memo), blink(str(int(tail)), memo)]
-            })
-        else:
-            memo.update({stone: [str(int(stone)*2024)]})
-
+    if stone in memo:
         return memo[stone]
 
-    memo = {"0": ["1"]}
-    for i in range(1):
+    if len(stone) % 2 == 0:
+        head = stone[:len(stone)//2]
+        tail = stone[len(stone)//2:]
+        memo.update({
+            stone: [
+                str(int(head)),
+                str(int(tail)),
+            ]
+        })
+    else:
+        memo.update({stone: [str(int(stone)*2024)]})
+
+    return memo[stone]
+
+
+def part1(stones, n, memo=None):
+    if memo is None:
+        memo = {"0": "1"}
+    for i in range(n):
         newline = []
         for stone in stones:
-            newline += blink(stone, memo)
-
+            new = blink(stone, memo)
+            newline += new if type(new) == list else [new]
         stones = newline
-        print("i:", stones)
+        # print(f"{i+1}:", stones)
 
-    return
+    return stones
 
 
-if __name__ == "__main__":
-    # assert len(part1(example, 25)) == 55312
-    part2(["125", "17"], 6)
+print(part1(["125", "17"], 6))
+                    
